53fd8ae18daa29e3e69ccfc527458df23e91f9f9
angelaleone01 <angelajinleone@gmail.com>
2024-11-11 14:55:49 -0600

    ride event card and adding types folder

---
+++ src/api/types/RideEvent.ts
@@ -0,0 +1,9 @@
+import type { TimeObj } from '@vuepic/vue-datepicker'
+
+export type RideEvent = {
+  rideEventID: number
+  location: string
+  date: string //TODO change this to date
+  time: string //might have to change this to hours, minutes
+  name: string
+}
